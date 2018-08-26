1fcc9841a8c8e476177f70b40b24d02cd04771cc
Debo <edery.deborah@gmail.com>
2018-08-26 13:36:38 +0300

    Openes site Ebay

---
+++ FirstWithGit/src/test/java/EbayTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class EbayTest
+{
+
+    ChromeDriver driver;
+ @Test
+    public void setUp()
+    {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://www.ebay.com");
+        
+        driver.quit();
+    }
+}
